101888ec487a9e57e56651c7dda6dbe0fd53cf94
Lei Pan <leip@google.com>
2023-12-11 18:22:03 -0800

    init checkin

---
+++ env-setup/terraform/gke.tf
@@ -0,0 +1,154 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# google_client_config and kubernetes provider must be explicitly specified like the following.
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${module.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.gke.ca_certificate)
+}
+
+module "gke" {
+  source                     = "terraform-google-modules/kubernetes-engine/google"
+  project_id                 = data.google_project.project.project_id
+  name                       = var.cluster_name
+  release_channel            = var.gke_release_channel
+  regional                   = false 
+  zones                      = [var.zone]
+  network                    = google_compute_network.cluster_network.name
+  subnetwork                 = google_compute_subnetwork.cluster_subnetwork.name
+  ip_range_pods              = google_compute_subnetwork.cluster_subnetwork.secondary_ip_range.1.range_name
+  ip_range_services          = google_compute_subnetwork.cluster_subnetwork.secondary_ip_range.0.range_name
+  default_max_pods_per_node  = var.max_pods_per_node
+  remove_default_node_pool   = true
+  http_load_balancing        = false
+  network_policy             = false
+  horizontal_pod_autoscaling = true
+  filestore_csi_driver       = false
+  create_service_account     = true 
+  grant_registry_access      = true  
+  identity_namespace         = "${data.google_project.project.project_id}.svc.id.goog" 
+
+  monitoring_enable_managed_prometheus = true 
+  
+  cluster_resource_labels = { "mesh_id" : "proj-${data.google_project.project.number}" }
+
+  node_pools = [
+    {
+      name                      = "cpu-node-pool"
+      machine_type              = "e2-medium"
+      node_locations            = var.zone
+      min_count                 = var.default_pool_node_count 
+      max_count                 = var.default_pool_node_count
+      local_ssd_count           = 0
+      spot                      = false
+      disk_size_gb              = 100
+      disk_type                 = "pd-standard"
+      image_type                = "COS_CONTAINERD"
+      enable_gcfs               = false
+      enable_gvnic              = false
+      auto_repair               = true
+      auto_upgrade              = true
+      preemptible               = false
+    },
+    {
+      name                      = "triton-node-pool" 
+      machine_type              = var.machine_type 
+      accelerator_type          = var.accelerator_type
+      accelerator_count         = var.accelerator_count
+      node_locations            = var.zone
+      min_count                 = var.triton_node_count
+      max_count                 = var.triton_node_count
+      local_ssd_count           = 0
+      spot                      = false
+      disk_size_gb              = var.disk_size_gb
+      disk_type                 = var.disk_type
+      image_type                = "COS_CONTAINERD"
+      enable_gcfs               = false
+      enable_gvnic              = true
+      auto_repair               = true
+      auto_upgrade              = true
+      preemptible               = false
+    },
+  ]
+
+  node_pools_oauth_scopes = {
+    all = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    triton-node-pool = [
+        "https://www.googleapis.com/auth/cloud-platform", 
+    ]
+  }
+
+  node_pools_labels = {
+    all = {}
+
+    cpu-node-pool = {
+      default-node-pool = true
+    }
+
+    triton-node-pool = {
+      gpu-node-pool = true
+    }
+  }
+
+  node_pools_taints = {
+    all = []
+
+    triton-node-pool = [
+      {
+        key    = "triton-node-pool"
+        value  = true
+        effect = "PREFER_NO_SCHEDULE"
+      },
+    ]
+  }
+}
+
+
+resource "kubernetes_namespace" "triton_namespace" {
+  metadata {
+    name = var.triton_namespace
+  }
+}
+
+
+module "triton_workload_identity" {
+  source       = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+  project_id   = data.google_project.project.project_id
+  name         = var.triton_sa_name 
+  namespace    = kubernetes_namespace.triton_namespace.metadata[0].name 
+  roles        = var.triton_sa_roles
+}
+
+
+module "asm" {
+  source                    = "terraform-google-modules/kubernetes-engine/google//modules/asm"
+  project_id                = data.google_project.project.project_id
+  cluster_name              = module.gke.name
+  cluster_location          = module.gke.location
+  enable_cni                = false
+  enable_fleet_registration = true
+  enable_mesh_feature       = false 
+  channel                   = var.asm_release_channel
+}
+
+
+
---
+++ env-setup/terraform/variables.tf
@@ -0,0 +1,144 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "region" {
+    description = "The region for the environment resources"
+    type        = string
+}
+
+variable "zone" {
+    description = "The zone for a Vertex Notebook instance"
+    type        = string
+}
+
+
+variable "repository_bucket_name" {
+    description = "The GCS bucket name to be used as model repository"
+    type        = string
+}
+
+variable "force_destroy" {
+    description = "Force destroy flag for the GCS bucket"
+    default = true 
+}
+
+variable "cluster_name" {
+    description = "The name of the GKE cluster"
+    type        = string
+}
+
+variable "cluster_description" {
+    description = "The cluster's description"
+    default = "GKE cluster to host NVIDIA Triton"
+}
+
+
+variable "triton_node_count" {
+    description = "The GPU pool node count"
+    default     = 1
+}
+
+variable "default_pool_node_count" {
+    description = "The number of nodes in the default node pool"
+    default     = 3
+}
+
+variable "disk_size_gb" {
+    description = "Disk size on a Triton node"
+    default = 500
+}
+
+variable "disk_type" {
+    description = "Disk typ on a Triton node"
+    default = "pd-standard"
+}
+
+variable "triton_sa_name" {
+    description = "The service account name for Triton workload identity."
+    default = "triton-sa"
+}
+
+variable "triton_sa_roles" {
+  description = "The roles to assign to the Triton service account"
+  default = [
+    "roles/storage.objectViewer",
+    ] 
+}
+
+variable "triton_namespace" {
+    description = "The K8s namespace for Triton deployment."
+    default = "default"
+}
+
+
+variable "network_name" {
+    description = "The network name"
+    type        = string
+}
+
+variable "subnet_name" {
+    description = "The subnet name"
+    type        = string
+}
+
+variable "subnet_ip_range" {
+  description = "The IP address range for the subnet"
+  default     = "10.129.0.0/20"
+}
+
+variable "pods_ip_range" {
+    description = "The secondary IP range for pods"
+    default     = "192.168.64.0/20"
+}
+
+variable "services_ip_range" {
+    description = "The secondary IP range for services"
+    default     = "192.168.80.0/20"
+}
+
+variable "max_pods_per_node" {
+    description = "The maximum number of pods to schedule per node"
+    default     = 110 
+}
+
+variable "machine_type" {
+    description = "The machine type for the Triton node pool"
+    default = "n1-standard-4"
+}
+
+variable "accelerator_type" {
+    description = "Type of accelerator (GPUs) for the Triton node pool"
+    default = "nvidia-tesla-t4"
+}
+
+variable "accelerator_count" {
+    description = "Number of accelerator(s) (GPUs) for the Triton node pool"
+    default = "1"
+}
+
+variable "gke_release_channel" {
+    description = "GKE release channel"
+    default = "STABLE"
+}
+
+variable "asm_release_channel" {
+    description = "GKE release channel"
+    default = "stable"
+}
